b75c9e3ee479f86ff19fad54c48fd70587962474
矢田貝諒 <yattun@yatakairyou-no-MacBook-Pro.local>
2016-09-06 22:11:07 +0900

    ２章まで

--- app/controllers/hello_controller.rb
+++ app/controllers/hello_controller.rb
@@ -1,2 +1,5 @@
 class HelloController < ApplicationController
+  def list
+    @books = Book.all
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  match ':controller(/:action(/:id))', via: [ :get, :post, :patch]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
